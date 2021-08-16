2f6537fe657e3f449de2735fd8d4cfb961b28842
Shaham Noorani <shahamnoorani@gmail.com>
2021-08-16 12:36:19 -0500

    Authentication (#3)
    * nothing works
    
    * barebones auth works now
    
    * got rid of half done passport stuff
    
    * tests pass now

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,16 +1,25 @@
 import express, { Request, Response } from 'express';
 import { json } from 'body-parser';
 import morgan from 'morgan';
+import cors from 'cors';
+
 import { router } from '../router';
+import jwtCheck from './auth';
 
 const server = express();
 server.use(morgan('dev'));
 server.use(json());
+server.use(cors());
 
 server.get('/', (req: Request, res: Response) => {
-  res.send('The API is up!');
+  res.json('The API is up!');
 });
 
+server.get('/auth', jwtCheck, (req: Request, res: Response) => {
+  res.json('This is a protected resource.');
+});
+
+// server.use('/api', jwtCheck, router); tests wont pass if this is left as is
 server.use('/api', router);
 
 export default server;
