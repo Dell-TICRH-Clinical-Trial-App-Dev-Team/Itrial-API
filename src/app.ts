1bd34b0c2eec895c7e00e42cf4f85ba33be17f16
Steven Xu <stevendoesstuffs@protonmail.com>
2021-08-20 18:22:26 -0500

    move over to eslint

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import server from './config/server';
-import { connectToDB } from './config/db';
-import config from './config/config';
-
-connectToDB().then(() =>
-  server.listen(config.port, () => {
-    console.log('Listening on http://localhost:' + config.port);
-  })
-);
